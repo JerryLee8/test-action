b84d6ac703906f4b48dd232a2ca4d22dfd760884
JerryLee8 <101862809+JerryLee8@users.noreply.github.com>
2022-06-19 21:11:25 +0800

    123

--- citysV2.py
+++ citysV2.py
@@ -36,14 +36,8 @@ driver.get("https://www.accuweather.com/zh/browse-locations/asi/tw")
 
 wait = WebDriverWait(driver, 5)  # 等待10秒
 
-## BS4 爬蟲
-str1=driver.page_source
-soup=BeautifulSoup(str1, "html.parser")
-soup1 = soup.select('.result-container')
-soup2 = soup1[0].select('.search-result')
-#print(soup2)
 
-elem = driver.find_element(by=By.LINK_TEXT, value=soup2[0].string)
+elem = driver.find_element(by=By.CLASS_NAME, value="search-result")
 elem.click()
 time.sleep(2)  # 等1sec
 try:
